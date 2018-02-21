eeaea2f20cd3901f7aef77781014c69e3bd52c7d
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-21 20:20:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function solveEquation(equation) {
-  let solution = equation.replace(/\s/g, '').match(/[^\^]\d+/g)
+  let mult = equation.replace(/\s/g, '').match(/[^\^]\d+/g)
   let answer = [0, 0]
 
-  let det = Math.sqrt(solution[1]*solution[1] - 4*solution[0]*solution[2])
-  answer[0] = Math.round( (-1)*solution[1] - det ) / (2*solution[0])
-  answer[1] = Math.round( (-1)*solution[1] + det ) / (2*solution[0])
+  let det = Math.sqrt(mult[1]*mult[1] - 4*mult[0]*mult[2])
+  answer[0] = Math.round( (-1)*mult[1] - det ) / (2*mult[0])
+  answer[1] = Math.round( (-1)*mult[1] + det ) / (2*mult[0])
   answer.sort((l, r) => {return l-r} )
   return answer
 }
