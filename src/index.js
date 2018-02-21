28e4e0eee9351d11801ecdb343cf7888e1e27b00
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-21 20:17:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let solution = equation.replace(/\s/g, '').match(/[^\^]\d+/g)
+  let answer = [0, 0]
+
+  let det = Math.sqrt(solution[1]*solution[1] - 4*solution[0]*solution[2])
+  answer[0] = Math.round( (-1)*solution[1] - det ) / (2*solution[0])
+  answer[1] = Math.round( (-1)*solution[1] + det ) / (2*solution[0])
+  answer.sort((l, r) => {return l-r} )
+  return answer
 }
